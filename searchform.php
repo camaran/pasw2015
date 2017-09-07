a378f848c04dd0b9c6b5391cc819f398f616a375
ChatMe <camaran@gmail.com>
2017-09-07 15:04:32 +0200

    usiamo button

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>/">
 <div>
     <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
-    <input type="submit" id="searchsubmit" value="cerca" />
+    <button type="submit" id="searchsubmit">Cerca</button>
 </div>
 </form>
